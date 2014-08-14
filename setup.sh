460e30c131dd966ee01150e1d3112dda84978e23
Russ Hewgill <RussHewgill@users.noreply.github.com>
2014-08-14 14:00:28 -0700

    dir error

--- setup.sh
+++ setup.sh
@@ -6,7 +6,9 @@ ln -rs vim/.vimrc ~/.vimrc
 mv ~/.zshrc{,.bak}
 ln -rs .zshrc ~/.zshrc
 
-mkdir ~/.vim
+if [ ! -d "$HOME/.vim" ]; then
+    mkdir ~/.vim 
+fi
 git clone https://github.com/gmarik/Vundle.vim.git ~/.vim/bundle/Vundle.vim
 
 
