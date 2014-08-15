063ab804f4706030f166a46ff413358d111d0005
Russ Hewgill <RussHewgill@users.noreply.github.com>
2014-08-15 12:10:55 -0700

    wrong loop

--- setup.sh
+++ setup.sh
@@ -15,10 +15,9 @@ fi
 
 if [ ! -d "$HOME/.vim" ]; then
     git clone https://github.com/gmarik/Vundle.vim.git ~/.vim/bundle/Vundle.vim 
-    yaourt --noconfirm prezto-git
 fi
 
-
+yaourt prezto-git
 
 
 
